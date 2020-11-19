83055144b581cacd93583040af2296800cf05209
Ishtiaque Khan <ishtiaque.khan@newscred.com>
2020-11-19 20:42:05 +0600

    Add classnames package

--- understand-quran/code/components/Header.tsx
+++ understand-quran/code/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import classNames from 'classnames';
 import { Avatar, Link as MaterialLink, Typography } from '@material-ui/core';
 
 import styles from './header.module.scss';
@@ -12,7 +13,10 @@ type HeaderProps = {
 };
 
 const Header: FC<HeaderProps> = ({ isHome }) => (
-  <header className={styles['uq-Header']}>
+  <header className={classNames({
+    [styles['uq-Header']]: true,
+    [styles['uq-Header--home']]: isHome,
+  })}>
     {isHome ? (
       <>
         <Avatar alt={name} src="/images/profile.jpg" />
