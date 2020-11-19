42db40c4ea4cb1ef576f1bbf267912ad8f82deb0
Ishtiaque Khan <ishtiaque.khan@newscred.com>
2020-11-19 20:17:51 +0600

    Add path alias for design library

--- understand-quran/code/next.config.js
+++ understand-quran/code/next.config.js
@@ -1,1 +1,9 @@
-module.exports = {}
+const path = require('path');
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias['@lib'] = path.resolve(__dirname, 'lib/');
+
+    return config
+  },
+}
